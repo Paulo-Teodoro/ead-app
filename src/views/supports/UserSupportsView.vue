40e5a8cf06fcbed2f005e7367d817b1e322777e3
Paulo Teodoro <paulolimrs@outlook.com>
2022-05-25 22:33:59 -0300

    Create User Supports Page

--- src/views/supports/UserSupportsView.vue
+++ src/views/supports/UserSupportsView.vue
@@ -1,13 +1,47 @@
 <template>
-  <div class="home">
-    Second Page - User Supports
+  <div>
+    <div class="pageTitle">
+      <span class="title">Minhas Duvidas</span>
+      <span class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+    </div>
+
+    <div class="content">
+      <div class="container">
+        <div class="left">
+          <div class="card">
+            <div class="title bg-laravel">
+              <span class="text">Filtros</span>
+            </div>
+            <div class="modules">
+              <ul class="classes">
+                <li>Todos</li>
+                <li>Aguardando Minha Resposta</li>
+                <li>Aguardando Professor</li>
+                <li>Finalizados</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="right">
+          <div class="content">
+            <div class="comments">
+              <supports-component />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-
+import SupportsComponent from "@/components/SupportsComponent.vue";
 export default {
-  name: 'UserSupportsView',
-}
+  name: "UserSupportsView",
+  components: { SupportsComponent },
+};
 </script>
