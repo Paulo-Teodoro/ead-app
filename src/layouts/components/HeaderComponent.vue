d30175445ab8d93daa1482214e42e5a4f0f8fa52
Paulo Teodoro <paulolimrs@outlook.com>
2022-06-07 23:22:28 -0300

    create side menu

--- src/layouts/components/HeaderComponent.vue
+++ src/layouts/components/HeaderComponent.vue
@@ -1,16 +1,38 @@
 <template>
     <header id="header">
         <nav>
-            <span class="toggleMenu far fa-bars"></span>
+            <span class="toggleMenu far fa-bars" @click="toggleMenu()"></span>
             <span class="logo">
                 <img :src="require('@/assets/images/logo.svg')" alt="EspecializaTI">
             </span>
         </nav>
     </header>
+
+    <side-menu-component v-show="showMenu">
+    </side-menu-component>
 </template>
 
 <script>
+import { ref } from 'vue'
+
+import SideMenuComponent from './SideMenuComponent.vue'
+
 export default {
-    name: 'header-component'
+    name: 'header-component',
+
+    setup() {
+        const showMenu = ref(false)
+
+        const toggleMenu = () => showMenu.value = !showMenu.value
+
+        return {
+            showMenu,
+            toggleMenu
+        }
+    },
+
+    components: {
+        SideMenuComponent
+    }
 }
 </script>
